ebdee9dd4168ba68dea997dfbca2960aa6113c02
Laura Higgins <lauraghiggins@me.com>
2017-03-14 12:47:51 -0700

    User can view description. #3

--- main.js
+++ main.js
@@ -1,9 +1,53 @@
-var images = ['candle1', 'candle2', 'candle3', 'candle4', 'candle5', 'candle6']
+var candles = [
+  {
+    name: 'Peace Becomes Her',
+    description: 'Fend off death with this peaceful mixture of aged juniper and winter cranberry. Burns for 100 hours. Wood Wick.',
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    name: 'Serentiy, NOW!',
+    description: 'Don\'t beat around the bush, get serene NOW. Peppermint and basil medle together for ahh..serenity. Burns for 100 hours. Wood Wick.',
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    name: 'Shangri-lovely',
+    description: 'Madarin and vanilla channel the scents of Shangria-La, whisking you away to the jungle. Burns for 100 hours. Wood Wick',
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    name: 'Aloahhhh',
+    description: 'Feel like your beachside in Hawaii with coconut and sugar filling the air. Burns for 110 hours. Wood Wick.',
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    name: 'The Office',
+    description: 'Maybe you\'re most relaxed at work. This clean, sterile mixture of mint and cotton will bring you back to your desk, feeling productive. Burns for 100 hours. Wood Wick.',
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    name: 'Meadow Spring',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood Wick.',
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+]
 
-for(i = 0; i<images.length; i++) {
-  var $imageContainer = document.createElement('div')
-  $imageContainer.classList.add('col-sm-6')
-  $imageContainer.textContent = images[i]
+for(i = 0; i<candles.length; i++) {
+  var $candleContainer = document.createElement('div')
+  $candleContainer.classList.add('col-sm-6')
+
+  var $name = document.createElement('div')
+  $name.textContent = candles[i].name
+
+  var $picture = document.createElement('img')
+  $picture.src = candles[i].source
+
+  var $description = document.createElement('div')
+  $description.textContent = candles[i].description
+
   var $gallery = document.querySelector('#gallery')
-  $gallery.appendChild($imageContainer)
+  $gallery.appendChild($candleContainer)
+  $candleContainer.appendChild($name)
+  $candleContainer.appendChild($picture)
+  $candleContainer.appendChild($description)
+
 }
