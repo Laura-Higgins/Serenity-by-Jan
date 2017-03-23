1ca583b9214c6c4dc9141edfae48433710a49fef
Laura Higgins <lauraghiggins@me.com>
2017-03-23 10:02:00 -0700

    established view changer, cleaned functions, added title

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var candles = [
   {
     id: 0,
     name: 'Peace Becomes Her',
-    description: 'Fend off death with this peaceful mixture of aged juniper and winter cranberry. Burns for 100 hours. Wood wick.',
+    description: 'Fend off death with this peaceful mixture of aged juniper and winter cranberry. Burns for 100 hours. Wood wick. ',
     price: 10.00,
     source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
   },
@@ -41,6 +41,48 @@ var candles = [
     price: 10.00,
     source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
   },
+  {
+    id: 6,
+    name: 'Spring Meadow',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood wick.',
+    price: 10.00,
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1073481.jpg'
+  },
+  {
+    id: 7,
+    name: 'Spring Meadow',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood wick.',
+    price: 10.00,
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    id: 8,
+    name: 'Spring Meadow',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood wick.',
+    price: 10.00,
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1073481.jpg'
+  },
+  {
+    id: 9,
+    name: 'Spring Meadow',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood wick.',
+    price: 10.00,
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
+  {
+    id: 10,
+    name: 'Spring Meadow',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood wick.',
+    price: 10.00,
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1073481.jpg'
+  },
+  {
+    id: 11,
+    name: 'Spring Meadow',
+    description: 'Bambi was rarely stressed. Feel like a fawn in Spring with this delicious lavander and orange mixture. Burns for 110 hours. Wood wick.',
+    price: 10.00,
+    source: 'http://www.yankeecandle.com/media/images/product/medium/1351702.jpg'
+  },
 ]
 var cart = []
 var $cartIcon = document.querySelector('#cartIcon')
@@ -51,39 +93,57 @@ var $count = document.querySelector('#count')
 var $checkoutForm = document.querySelector('#checkoutForm')
 var $submit = document.querySelector('#submit')
 var $closeOrder = document.querySelector('#closeOrder')
+var $description = document.querySelector('#description')
+var views = [$gallery, $checkout, $description]
+
 
 for(var i = 0; i < candles.length; i++) {
-  createCandleItem(i);
+  createCandleItem(candles[i]);
 }
 
-document.addEventListener('click', function(event) {
-  var id = event.target.getAttribute('data-id')
+function changeView(view) {
+  views.forEach(function (v) {
+    v.classList.add('hidden')
+  })
+  view.classList.remove('hidden')
+}
 
-  if(id) {
-    var $description = document.querySelector('div[data-id="' + id + '"]')
-     $description.classList.remove('invisible')
-  }
+document.querySelectorAll(".show-description").forEach(function ($button) {
+  $button.addEventListener('click', function(event) {
+    var id = event.target.getAttribute('data-id')
+    var candle = findCandle(parseInt(id));
+
+    if(parseInt(id) === candle.id) {
+      showCandle(candle)
+      changeView($description)
+
+    }
+  })
 })
 
-document.addEventListener('click', function(event) {
-  var $button = event.target
+$description.addEventListener('click', function (event) {
+  if(event.target.tagName === 'BUTTON') {
 
-  if($button.tagName !== 'BUTTON') {
-    return
-  } else {
-    var $buttonId = $button.getAttribute('data-id')
-  }
-  for (var i = 0; i < candles.length; i++) {
-    if(parseInt($buttonId) === candles[i].id) {
-      cart.push(candles[i])
-      $count.textContent = cart.length
-      cartTotal()
+    var buttonId = event.target.getAttribute('data-id')
+
+    if(buttonId === 'goback2') {
+      changeView($gallery)
+
+    } else {
+
+      for(var i = 0; i< candles.length; i++) {
+        if(parseInt(buttonId) === candles[i].id) {
+          cart.push(candles[i])
+          $count.textContent = cart.length
+          cartTotal(cart)
+        }
+      }
     }
   }
 })
 
 $cartIcon.addEventListener('click', function(event) {
-  changeWindowCheckout()
+  changeView($checkout)
 })
 
 $checkoutForm.addEventListener('submit', function(event) {
@@ -95,12 +155,12 @@ $closeOrder.addEventListener('click', function(event) {
   cart = []
   $count.textContent = cart.length
   $checkoutForm.reset()
-  changeWindowHome()
   cartTotal()
+  changeView($gallery)
 })
 
 $goback.addEventListener('click', function(event) {
-  changeWindowHome()
+  changeView($gallery)
 })
 
 function cartTotal() {
@@ -113,49 +173,76 @@ function cartTotal() {
   $userTotal.textContent = total
 }
 
-function changeWindowHome() {
-  $gallery.classList.remove('hidden')
-  $checkout.classList.add('hidden')
-}
-
-function changeWindowCheckout() {
-  $gallery.classList.add('hidden')
-  $checkout.classList.remove('hidden')
-}
-
-function createCandleItem(i) {
+function createCandleItem(candle) {
 
   var $candleContainer = document.createElement('div')
-  $candleContainer.classList.add('col-sm-6', 'text-center')
+  $candleContainer.classList.add('col-sm-4', 'text-center')
 
   var $name = document.createElement('div')
-  $name.textContent = candles[i].name
+  $name.textContent = candle.name
   $name.classList.add('title')
 
   var $picture = document.createElement('img')
-  $picture.src = candles[i].source
-  $picture.setAttribute('data-id', candles[i].id)
+  $picture.src = candle.source
 
-  var $description = document.createElement('div')
-  $description.textContent = candles[i].description
-  $description.setAttribute('data-id', candles[i].id)
-  $description.classList.add('invisible')
-  $description.classList.add('text-center')
+  var $viewDetailsButton = document.createElement('button')
+  $viewDetailsButton.textContent = "view more details"
+  $viewDetailsButton.classList.add("button", "show-description")
+  $viewDetailsButton.setAttribute('data-id', candle.id)
 
-  var $price = document.createElement('div')
-  $price.textContent = candles[i].price
-  $price.classList.add('text-center')
-  $price.classList.add('price')
-
-  var $button = document.createElement('button')
-  $button.textContent = "Add to cart"
-  $button.setAttribute('class', "button")
-  $button.setAttribute('data-id', candles[i].id)
 
   $gallery.appendChild($candleContainer)
   $candleContainer.appendChild($name)
   $candleContainer.appendChild($picture)
-  $candleContainer.appendChild($description)
-  $description.appendChild($price)
-  $description.appendChild($button)
+  $candleContainer.appendChild($viewDetailsButton)
+}
+
+function findCandle(id) {
+  for(var i = 0; i < candles.length; i++) {
+    if (candles[i].id === id) {
+      return candles[i]
+    }
+  }
+}
+
+function showCandle(candle) {
+  var $deletePrevious = document.querySelector('.descriptionContainer')
+  if($deletePrevious) {
+  $deletePrevious.parentNode.removeChild($deletePrevious)
+}
+
+  var $descriptionContainer = document.createElement('div')
+    $descriptionContainer.classList.add("text-center", "descriptionContainer")
+
+  var $picture = document.createElement('img')
+  $picture.src = candle.source
+
+  var $Candledescription = document.createElement('div')
+  $Candledescription.textContent = candle.description
+
+  var $title = document.createElement('div')
+  $title.textContent = candle.name
+  $title.classList.add('title')
+
+  var $price = document.createElement('div')
+  $price.textContent = "$"+ candle.price + ".00"
+  $price.classList.add('price')
+
+  var $addToCartButton = document.createElement('button')
+  $addToCartButton.textContent = "add to cart!"
+  $addToCartButton.setAttribute('data-id', candle.id)
+  $addToCartButton.classList.add("button")
+
+  var $goback2 = document.createElement('button')
+  $goback2.textContent = 'return to main page'
+  $goback2.setAttribute('data-id', 'goback2')
+  $goback2.classList.add('button')
+
+  $description.appendChild($descriptionContainer)
+  $descriptionContainer.appendChild($title)
+  $descriptionContainer.appendChild($picture)
+  $descriptionContainer.appendChild($Candledescription)
+  $descriptionContainer.appendChild($price)
+  $descriptionContainer.appendChild($addToCartButton)
+  $descriptionContainer.appendChild($goback2)
 }
